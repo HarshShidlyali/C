6cc5bfc5e1e60b21d176a638d76e894e3ac4f25b
Harsh Shidlyali <135652215+HarshShidlyali@users.noreply.github.com>
2025-02-25 23:44:40 +0530

    Create ASCII_value.c

---
+++ Easy/ASCII_value.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter the Character to get the ASCII Value : " );
+    scanf("%c",&ch);
+    printf("\nThe ASCII value is : %d",ch);
+    return 0;
+}
